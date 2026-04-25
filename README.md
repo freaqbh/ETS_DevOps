13517b88c2739f7552addbd0079b141a6e01b9d3
freaqbh <alfaruqiabhi111@gmail.com>
2026-04-25 14:28:56 +0700

    test jenkins

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-test
-
 # devops-miniproject-3-kel1
 
 # 🚀 Enterprise CI/CD Pipeline - End-to-End DevOps Automation
--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World! Deploy Otomatis via JCasC Berhasil!');
+    res.send('Hello World! Deploy Otomatis via JCasC Berhasil! 2');
 });
 
 // Endpoint untuk smoke test post-deployment
